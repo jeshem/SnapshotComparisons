3482c484e0196457500cb91a42ee01f348e3dec4
Shem Cheng <scheng@netsuite.com>
2020-03-11 17:02:23 -0700

    looks for latest files

--- SnapshotComparisons/PrintLimitOutput.py
+++ SnapshotComparisons/PrintLimitOutput.py
@@ -1,14 +1,16 @@
 from openpyxl import Workbook
+from datetime import datetime
+import os
 
-def print_limits(data):
-    wb = Workbook()
-    FRA_ws = wb.create_sheet('FRA')
-    PHX_ws = wb.create_sheet('PHX')
-    IAD_ws = wb.create_sheet('IAD')
+class limit_output_and_compare(object):
+    date = datetime.date(datetime.now())
 
     FRA_limits = []
     PHX_limits = []
     IAD_limits = []
+
+    title = 'NS-OCI Limits '
+    latest_file = ''
 
     keys = [
 	    'name',
@@ -22,35 +24,67 @@ def print_limits(data):
         'region_name'
     ]
 
-    print(wb.sheetnames)
+    
+    def __init__(self, location, limits):
+        self.loc = location
 
+        for things in limits:
+            if things['region_name'] == 'eu-frankfurt-1':
+                self.FRA_limits.append(things)
+            elif things['region_name'] == 'us-phoenix-1':
+                self.PHX_limits.append(things)
+            elif things['region_name'] == 'us-ashburn-1':
+               self.IAD_limits.append(things)
 
-    for things in data:
-        if things['region_name'] == 'eu-frankfurt-1':
-            FRA_limits.append(things)
-        elif things['region_name'] == 'us-phoenix-1':
-            PHX_limits.append(things)
-        elif things['region_name'] == 'us-ashburn-1':
-           IAD_limits.append(things)
+        pass
 
-    for row in range(len(FRA_limits)):
-        thing = FRA_limits[row]
-        for col in range(len(keys)):
-            key = keys[col]
-            FRA_limits.cell(row, col).value = thing[key]
+    def print_limits(self):
+        wb = Workbook()
+        FRA_ws = wb.active
+        FRA_ws.title = 'FRA'
+        PHX_ws = wb.create_sheet('PHX')
+        IAD_ws = wb.create_sheet('IAD')
+        
+        for row in range(len(FRA_limits)):
+            thing = self.FRA_limits[row]
+            for col in range(len(keys)):
+                self.key = keys[col]
+                FRA_ws.cell(row, col).value = thing[key]
 
-    for row in range(len(PHX_limits)):
-        thing = PHX_limits[row]
-        for col in range(len(keys)):
-            key = keys[col]
-            PHX_limits.cell(row, col).value = thing[key]
+        for row in range(len(PHX_limits)):
+            thing = self.PHX_limits[row]
+            for col in range(len(keys)):
+                self.key = keys[col]
+                PHX_ws.cell(row, col).value = thing[key]
 
-    for row in range(len(IAD_limits)):
-        thing = IAD_limits[row]
-        for col in range(len(keys)):
-            key = keys[col]
-            IAD_limits.cell(row, col).value = thing[key]
+        for row in range(len(IAD_limits)):
+            thing = self.IAD_limits[row]
+            for col in range(len(keys)):
+                self.key = keys[col]
+                IAD_ws.cell(row, col).value = thing[key]
 
-    wb.save("test.xlsx")
+        wb.save(self.title + self.date + ".xlsx")
 
+    #find new files and insert them into a list
+    def find_new_files(self):
+        dirs = os.listdir(self.loc)
+
+        last_date = self.date
+
+        for file in dirs:
+            #if the file is a directory, go into it and check for more files
+            if os.path.isdir(loc + "\\" + file):
+                        list = self.find_new_files(loc + "\\" + file, list)
+
+            #get the date the limits file was modified/created
+            file_create_date = os.stat(self.loc + "\\" + file).st_ctime
+            
+            #find the latest limits
+            if file_create_date > last_date:
+                last_date = file_create_date
+                self.latest_file = file
+
+    def compare_limits(self):
+
+        pass
 
--- SnapshotComparisons/SnapshotComparisons.py
+++ SnapshotComparisons/SnapshotComparisons.py
@@ -1,13 +1,16 @@
 from GetSnapshots import get_snapshot
-from PrintLimitOutput import print_limits
+from PrintLimitOutput import limit_output_and_compare
 
 def main():
 	limits = []
+	
+	location = "path"
 
 	snapshot = get_snapshot()
 	limits = snapshot.get_data_list()
 
-	print_limits(limits)
+	compare = limit_output_and_compare(location, limits)
+	compare.print_limits()
 
 
 if __name__ == "__main__":
